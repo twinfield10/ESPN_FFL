cb6d929c21d46c31ab7ac482577aa55ad89f0296
Tommy Winfield <tommy.winfield10@gmail.com>
2025-09-25 10:59:17 -0400

    Week 4 Commit

--- Scripts/scrape_BOL.py
+++ Scripts/scrape_BOL.py
@@ -24,7 +24,7 @@ week = NFL_SCHEDULE.filter(pl.col('away_score') == 'NA')['week'].min()
 print(f"Now Loading NFL Week {week}:")
 
 # BetOnline ID For First Game
-id_var = 259354
+id_var = 259370 #259386
 
 # Statistic Mapping
 stats = {
